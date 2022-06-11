96e77160ff4937d5fe3df63c832915631938d916
Craver <98490085+Craver123@users.noreply.github.com>
2022-06-11 23:35:46 +0200

    Add files via upload

---
+++ Retired.py
@@ -0,0 +1,111 @@
+from cgi import print_arguments
+from email.headerregistry import Address
+import multiprocessing
+from typing import Type
+import keyboard
+import base64
+import time
+import random
+import string
+import random
+import threading
+import ssl, os, requests
+from threading import active_count, Thread
+from pystyle import Colorate, Colors, Write
+from urllib.parse import urlparse
+from http import cookiejar
+import sys
+from email.mime import application
+from lib2to3.pgen2.driver import Driver
+from tkinter import *
+from tkinter.ttk import Combobox
+from tracemalloc import start
+from turtle import bgcolor, done, goto, onclick
+import os
+import tkinter as tk
+from tkinter import ttk 
+from selenium import webdriver
+import webbrowser
+import tkinter
+import os
+import sys
+import ctypes
+from tkinter import *
+import tkinter.messagebox
+import subprocess
+from distutils.cmd import Command
+from doctest import DocFileTest
+import ssl, os, requests, time
+from tkinter import mainloop
+from turtle import clear, title
+from threading import active_count, Thread
+from pystyle import Colorate, Colors, Write
+from random import randint, choice
+import os, requests, random, threading, json, time, multiprocessing
+from socket import timeout
+from colorama import Fore
+from urllib.parse import urlparse
+from http import cookiejar
+import sys
+import time
+import csv
+import time
+import sys
+import re
+import os
+import platform
+import re
+import sys
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+from time import monotonic
+import requests
+from colorama import init
+# Retired was proudly coded by Guilhem  .
+# Copyright (c) 2021 
+# Retired Nuker under the GNU General Public Liscense v2 (1991).
+os.system('cls')
+print(""""
+# Retired was proudly coded by Guilhem  .
+# Copyright (c) 2022 join https://discord.gg/h5xZrZ77Ss For help
+# Retired Nuker under the GNU General Public Liscense v2 (1991).
+""")
+time.sleep(3)
+os.system('cls')
+print("")
+print("")
+print(Colorate.Horizontal(Colors.green_to_white, f"        ▄████████    ▄████████     ███      ▄█     ▄████████    ▄████████ ████████▄ "))
+print(Colorate.Horizontal(Colors.green_to_white, f"       ███    ███   ███    ███ ▀█████████▄ ███    ███    ███   ███    ███ ███   ▀███              ____________________"))
+print(Colorate.Horizontal(Colors.green_to_white, f"       ███    ███   ███    █▀     ▀███▀▀██ ███▌   ███    ███   ███    █▀  ███    ███             /                    \ "))
+print(Colorate.Horizontal(Colors.green_to_white, f"      ▄███▄▄▄▄██▀  ▄███▄▄▄         ███   ▀ ███▌  ▄███▄▄▄▄██▀  ▄███▄▄▄     ███    ███             !    Craver Tool     !"))
+print(Colorate.Horizontal(Colors.green_to_white, f"     ▀▀███▀▀▀▀▀   ▀▀███▀▀▀         ███     ███▌ ▀▀███▀▀▀▀▀   ▀▀███▀▀▀     ███    ███             !    [+] - Nuk3r     !"))
+print(Colorate.Horizontal(Colors.green_to_white, f"     ▀███████████   ███    █▄      ███     ███  ▀███████████   ███    █▄  ███    ███             \____________________/"))
+print(Colorate.Horizontal(Colors.green_to_white, f"       ███    ███   ███    ███     ███     ███    ███    ███   ███    ███ ███   ▄███                      !  !"))
+print(Colorate.Horizontal(Colors.green_to_white, f"       ███    ███   ██████████    ▄████▀   █▀     ███    ███   ██████████ ████████▀                       !  !"))
+print(Colorate.Horizontal(Colors.green_to_white, f"       ███    ███                                 ███    ███                                              !  !"))
+print(Colorate.Horizontal(Colors.green_to_white, f"                                                                                                          !  !"))
+print(Colorate.Horizontal(Colors.green_to_white, f"                                                                                                          !  !"))
+print(Colorate.Horizontal(Colors.green_to_white, f"> Created by Craver                                                                                       !  !"))
+print(Colorate.Horizontal(Colors.green_to_white, f"> https://discord.gg/xWtNyujJDg                                                                           \__/"))
+time.sleep(3)
+print("")
+print("")
+print(Colorate.Horizontal(Colors.green_to_white, f"______________________________________________________________________________"))
+print(Colorate.Horizontal(Colors.green_to_white, f""))
+print(Colorate.Horizontal(Colors.green_to_white, f"[1] Nuke Account"))
+print("")
+print(Colorate.Horizontal(Colors.green_to_white, f"[2] Delete All Servers"))
+print("")
+print(Colorate.Horizontal(Colors.green_to_white, f"[3] Spam Create New Servers"))
+print("")
+print(Colorate.Horizontal(Colors.green_to_white, f"[4] Seizure Mode"))
+print("")
+sendType = int(Write.Input("[Choose Number]\n>> ", Colors.purple_to_blue, interval=0.0001))
+if sendType == (1):
+    print(Colorate.Horizontal(Colors.green_to_white, f"[+] Not Done"))
+elif sendType == (2):
+    print(Colorate.Horizontal(Colors.green_to_white, f"[+] Not Done"))
+elif sendType == (3):
+    print(Colorate.Horizontal(Colors.green_to_white, f"[+] Not Done"))
+elif sendType == (4):
+    print(Colorate.Horizontal(Colors.green_to_white, f"[+] Not Done"))
+time.sleep(3)
